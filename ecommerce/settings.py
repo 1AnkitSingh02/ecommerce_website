bd3f04736b79cc550ffcda08a4eef3414b64b2d9
1AnkitSingh02 <ankitsingh4301@gmail.com>
2020-12-27 23:43:55 +0530

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
