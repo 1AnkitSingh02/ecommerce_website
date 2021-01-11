361e5d957a28717077b65fd284b480926befc6b5
1AnkitSingh02 <ankitsingh4301@gmail.com>
2021-01-11 22:55:56 +0530

    added functionality to add in cart for guest user

--- store/views.py
+++ store/views.py
@@ -24,12 +24,39 @@ def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer= customer, complete=False)
-        item = order.orderitem_set.all()
+        items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print('Cart:',cart)
         items = []
         order = {'get_cart_total': 0 , 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]["quantity"])
+
+            order["get_cart_total"] += total
+            order["get_cart_items"] += cart[i]["quantity"]
+
+            item = {
+                'id':product.id,
+                'product':{
+                    'id':product.id,
+                    'name':product.name, 
+                    'price':product.price, 
+                        'imageURL':product.imageURL
+                    }, 
+                'quantity':cart[i]['quantity'],
+                'digital':product.digital,
+                'get_total':total,
+                }
+            items.append(item)
 
     context = {'items': items , 'order': order, 'cartItems' : cartItems}
     return render(request,'store/cart.html',context)
@@ -38,7 +65,7 @@ def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer= customer, complete=False)
-        item = order.orderitem_set.all()
+        items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
         items = []
